b0bf96e65e894b13a370e3f82b459f14851b5ec9
Thibaud Guillaume-Gentil <thibaud@thibaud.me>
2012-02-11 14:56:41 +0100

    debug

--- lib/listen/listener.rb
+++ lib/listen/listener.rb
@@ -194,7 +194,7 @@ module Listen
             new_stat = File.stat(path)
 
             p "stat: #{stat.mtime}"
-            # p "new_stat: #{new_stat.mtime}"
+            p "new_stat: #{new_stat.mtime}"
 
             if stat.mtime < new_stat.mtime || (stat.mtime.to_i == Time.now.to_i && content_modified?(path))
               @changes[:modified] << relative_path(path)
